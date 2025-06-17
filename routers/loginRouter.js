1fca3036aa758e1d66b7145ee2c3adae02b7f1f6
Nechama Krashinski <N.0556734722@gmail.com>
2025-06-17 12:38:46 +0300

    work

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const { registerClient, loginClient } = require('../controllers/clientController');
-const { authenticateToken } = require('../middleware/authMiddleware'); // ייבוא המידלוויר אם יש צורך להשתמש בו
+const { registerClient, loginClient } = require('../controllers/loginController');
 
 const router = express.Router();
 router.post('/register', registerClient);
-router.get('/login', loginClient);
+router.post('/login', loginClient);
 
 module.exports = router;
