2ac7ac57701e5ef8af6eb4f7d6a95515c5020ff0
Nechami0517 <nechami0517@gmail.com>
2025-06-17 10:41:13 +0300

    chane to use with .env

--- connection/dbConnection.js
+++ connection/dbConnection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 const BusinessConsulting = new Sequelize('BusinessConsulting',process.env.DB_USER, process.env.DB_PASSWORD, { // <--- כאן השינויים!
     host: process.env.DB_HOST || 'localhost', 
     dialect: 'mssql',
-    port: 1433,
+    port: process.env.DB_PORT || 1433, 
     dialectOptions: {
         encrypt: false, 
         trustServerCertificate: true 
